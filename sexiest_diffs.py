4ead8950f8cc50ee413505dbf1ff85a5c0ec2d6f
tengal_teemo <toby@zagmo.com>
2024-09-19 13:57:06 -0700

    fixup! implemented matthews persona approach with ashleys sexydiffs algorithm

--- sexiest_diffs.py
+++ sexiest_diffs.py
@@ -6,6 +6,8 @@ import re
 
 import numpy as np
 
+import tiktoken
+
 from pprint import pprint
 from wordllama import WordLlama
 from wordllama.wordllama import WordLlamaInference
@@ -39,29 +41,50 @@ def cosine_similarity(vec1: np.ndarray, vec2: np.ndarray):
         return 0
     return abs(np.dot(vec1, vec2.T) / (np.linalg.norm(vec1) * np.linalg.norm(vec2)))
 
+enc = tiktoken.get_encoding("o200k_base")
 
-def query_haiku(prompt: str, system_prompt: str):
+def query_haiku(prompt: str, system_prompt: str, temperature: float = 0.7):
     message = client.messages.create(
         model="claude-3-haiku-20240307",
         max_tokens=1024,
-        temperature=0.5,
+        temperature=temperature,
         system=system_prompt,
         messages=[
             {"role": "user", "content": prompt}
         ]
     )
-    return message.content
+    return message.content[0].text
 
 
-def summary_from_files(fv1p: str, fv2p: str, role_persona: str) -> list[dict]:
+def persona_from_role(role: str) -> str:
+    prompt = """A persona is a concise paragraph of text meant to encapsulate a person.  The purpose of this is that a persona paragraph can be embedded to allow for cosine similarity comparison of various characters, in order to determine relevance.  A persona shouldn't contain personal details like names, however they can contain characteristic adjectives.
+    Please create a persona for a <role>{role}</role>. Wrap it in <persona></persona> tags, and please be concise and begin the persona with the provided <role></role>.""".format(role=role)
+    system_prompt = f"You are Claude, an AI assistant, created to help summarize changes in documents in a helpful, harmless manner."
+    persona_response = query_haiku(prompt, system_prompt, temperature=0.7)
+    persona = re.search(r'<persona>((\n|.)*)</persona>', persona_response).groups()[0]
+    return persona
 
+
+def summary_from_files(fv1p: str, fv2p: str, role_personas: dict[str, str]) -> (list[dict], list[list[int]], int, int):
+    """
+
+    :param fv1p: path to v1 of the file
+    :param fv2p: path to v2 of the file
+    :param role_personas: list of personas to rank the relevant changes by
+    :return:
+        summarized diffs dict {'summary', 'persona', 'persona_embedding'}
+        persona map list, [[p1 most relevant -> p1 least relevant]...]
+        prompt tokens int
+        response tokens int
+    """
     if fv1p.split('.')[-1] != fv2p.split('.')[-1]:
         raise ValueError('file extensions don\'t match')
 
     fv1_text = file_to_plaintext(fv1p)[0].split('\n')
     fv2_text = file_to_plaintext(fv2p)[0].split('\n')
 
-    f_unified_diff = difflib.unified_diff(fv1_text, fv2_text)
+    # TODO: expand tokens around diffs if below certain threshold
+    f_unified_diff = difflib.unified_diff(fv1_text, fv2_text, n=2)
     f_unified_diffs = list(f_unified_diff)
 
     # Generate diffs
@@ -74,32 +97,67 @@ def summary_from_files(fv1p: str, fv2p: str, role_persona: str) -> list[dict]:
 
     summarized_diffs = []
 
+    prompt_tokens = 0
+    response_tokens = 0
+
+    system_prompt = f"You are Claude, an AI assistant, created to help summarize changes in documents in a helpful, harmless manner."
     for diff in diff_texts:
-        system_prompt = f"You are Claude, an AI assistant, created to help summarize changes in documents in a helpful, harmless manner."
+
         prompt = (f'Summarize changes in the unified diff present in the <edit></edit> tags.  Wrap the changes in some <summary></summary> tags.\n\n'
                   f'<edit>\n{diff}</edit>')
-        summary_response = query_haiku(prompt, system_prompt)[0].text
 
-        summary = re.match(r'<summary>((.|\n)*)</summary>', summary_response).groups()[0]
+        summary_response = query_haiku(prompt, system_prompt)
 
-        prompt = (f'Given the following text, in the given <text></text> tags, who is most likely to find it important?'
-                  f'Describe the persona concisely within <affects></affects> tags\n\n'
+        prompt_tokens += len(enc.encode(system_prompt)) + len(enc.encode(prompt))
+        response_tokens += len(enc.encode(summary_response))
+
+        summary = re.search(r'<summary>((.|\n)*)</summary>', summary_response).groups()[0]
+
+        prompt = (f'A persona is a concise description of a role someone might have, such as a CEO, Historian, Student.\n\n'
+                  f'Given the text below in the <text></text> tags, create a persona representing who would be interested in this text, devoid of personal details like names, describing the kind of person who would be interested.  Do this concisely within <persona></persona> tags\n\n'
                   f'<text>\n'
                   f'{summary}'
                   f'\n</text>')
-        persona_response = query_haiku(prompt, system_prompt)[0].text
+        persona_response = query_haiku(prompt, system_prompt)
 
-        persona = re.findall(r'<affects>(.*)</affects>', persona_response)
+        # counting the tokens
+        prompt_tokens += len(enc.encode(system_prompt)) + len(enc.encode(prompt))
+        response_tokens += len(enc.encode(persona_response))
+
+        persona = re.search(r'<persona>((.|\n)*)</persona>', persona_response).groups()[0]
 
         summarized_diffs.append({'summary': summary, 'persona': persona})
 
-    model = load_embedding_model()
-    persona_embedding = model.embed(role_persona)
+    persona_labels = list(role_personas.keys())
+    personas = list(role_personas.values())
+    persona_embeddings = [embed(role_persona) for role_persona in role_personas.values()]
 
     for count, summarized_diff in enumerate(summarized_diffs):
-        affects_similarities = cosine_similarity(persona_embedding, model.embed(summarized_diff['persona']))
-        summarized_diffs[count]['relevance'] = affects_similarities
+        summarized_diffs[count]['persona_embedding'] = embed(summarized_diff['persona'])
 
-    summarized_diffs.sort(key=lambda x: x['relevance'], reverse=True)
+    # with persona_embedding information, we now create an order of relevance per
+    persona_scores = []
+    for persona, embedding in zip(role_personas, persona_embeddings):
+        # this map will be sorted based on cosine similarity of persona embedding and index
+        scores = [cosine_similarity(summarized_diff['persona_embedding'], embedding) for summarized_diff in summarized_diffs]
+        persona_scores.append(scores)
 
-    return summarized_diffs
+    # combine relevant paragraphs
+    prompt_template = """Take the information below in the <change></change> tags, and summarize them in a concise, bullet-point list.\n
+    The summary should be contained in <summary></summary> tags.\n\n{changes}"""
+    threshold = 0.5
+    persona_summaries = {}
+    for persona_role, persona, persona_score in zip(role_personas, personas, persona_scores):
+        score_indices = list(range(len(persona_score)))
+        prompt_change = "\n\n".join([f'<change>\n{summarized_diffs[index]["summary"]}\n</change>' for index, score in zip(score_indices, persona_score) if float(score) > threshold])
+        if prompt_change:
+            prompt = prompt_template.format(changes=prompt_change)
+            prompt_tokens += len(enc.encode(system_prompt)) + len(enc.encode(prompt))
+            summary_response = query_haiku(prompt, system_prompt)
+            response_tokens += len(enc.encode(summary_response))
+            response_summary = re.search(r'<summary>((.|\n)*)</summary>', summary_response).groups()[0]
+        else:
+            response_summary = ""
+        persona_summaries.update({persona_role: response_summary})
+
+    return persona_summaries, prompt_tokens, response_tokens
--- test_sexiest_diffs.py
+++ test_sexiest_diffs.py
@@ -1,17 +1,46 @@
 import os
+import itertools
 
-from pprint import pprint
+from pandas import DataFrame
 
 from sexiest_diffs import summary_from_files
 
 
+persona_ceo = "A visionary and strategic CEO with two decades of experience in the tech industry. Leads a Fortune 500 company, driving digital transformation and sustainable growth. Charismatic public speaker, adept at navigating complex business landscapes, and passionate about fostering innovation. Champions diversity and inclusion in the workplace, known for inspirational leadership and ability to set long-term organizational direction."
+persona_cfo = "A meticulous and conservative CFO with an MBA from a top business school and extensive experience in investment banking. Excels in financial forecasting, risk management, and cost optimization. Ensures regulatory compliance and effectively communicates complex financial data to stakeholders. Known for securing favorable funding for company initiatives and maintaining a strong focus on fiscal responsibility and shareholder value."
+persona_engineer = "A talented and innovative Software Engineer specializing in full-stack development with expertise in cloud-native applications and DevOps practices. Passionate about clean code, test-driven development, and staying current with emerging technologies. Actively contributes to open-source projects and mentors junior developers. Possesses strong problem-solving skills and collaborates effectively across teams, making significant contributions to development projects."
+
 def main():
     test_path = 'test_files'
-    odt_v1_fp = 'v1.odt'
-    odt_v2_fp = 'v2.odt'
-    relevant_diffs = summary_from_files(os.path.join(test_path, odt_v1_fp), os.path.join(test_path, odt_v2_fp), 'Someone who loves the drums')
-    pprint(relevant_diffs)
+    test_files = [file for file in os.listdir(test_path) if not os.path.isdir(os.path.join(test_path, file))]
 
+    test_files_sorted = sorted(test_files, key=lambda x: x[0].lower())
+    test_files_grouped = {key: list(group) for key, group in itertools.groupby(test_files_sorted, key=lambda x: x[0].lower())}
+
+    personas = {'ceo': persona_ceo, 'cfo': persona_cfo, 'engineer': persona_engineer}
+
+    outfile = os.path.join(test_path, 'Outputs/', 'results1.csv')
+    if os.path.exists(outfile):
+        raise FileExistsError('Don\'t overwrite results!')
+
+    outdict = {'Input File': [], 'Output File': [], 'Prompt Tokens': [], 'Response Tokens': []}
+
+    for persona in personas.keys():
+        outdict.update({persona: []})
+
+    for file_versions in test_files_grouped.values():
+        file_versions.sort(key=lambda x: x[1].lower())
+        for first, second in zip(file_versions, file_versions[1:]):
+            persona_responses, prompt_tokens, response_tokens = summary_from_files(os.path.join(test_path, first), os.path.join(test_path, second), personas)
+            outdict['Input File'].append(first)
+            outdict['Output File'].append(second)
+            outdict['Prompt Tokens'].append(prompt_tokens)
+            outdict['Response Tokens'].append(response_tokens)
+            for role, response in persona_responses.items():
+                outdict[role].append(response)
+
+    df = DataFrame.from_dict(outdict)
+    df.to_csv(outfile)
 
 if __name__ == '__main__':
     main()
