46cf77c3099f8fd9e34c400db48bfd3946b10fcf
tengal_teemo <toby@zagmo.com>
2024-09-19 13:57:27 -0700

    updated api to expose sexiest_diffs.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import os
 
+from sexiest_diffs import summary_from_files, persona_from_role
+
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'uploads'
@@ -28,15 +30,18 @@ def handle_upload():  # put application's code here
     if len(earlier) != len(later):
         return 'Mismatched numbers of earlier and later files', 400
 
+    all_summaries = {}
+
     for earlier_file, later_file in zip(earlier, later):
         earlier_file.save(os.path.join(UPLOAD_FOLDER, earlier_file.filename))
         later_file.save(os.path.join(UPLOAD_FOLDER, later_file.filename))
 
-    # Handle Ashley's stuff
-    # generate_summary_from_file_diffs_and_role(file_paths, role)
+        # Handle Ashley's stuff
+        persona = persona_from_role(role)
+        role_summaries, prompt_tokens, response_tokens = summary_from_files(os.path.join(UPLOAD_FOLDER, earlier_file.filename), os.path.join(UPLOAD_FOLDER, later_file.filename), {role: persona})
+        all_summaries.update({later_file.filename: role_summaries[role]})
 
-    fake_summaries = ["Lorem", "Ipsum"]
-    return jsonify({"summaries": fake_summaries})
+    return jsonify({"summaries": all_summaries})
 
 
 if __name__ == '__main__':
